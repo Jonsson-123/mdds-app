9f9256b796508a86ec4d39be68b4f4c58d4f2090
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-09-06 20:50:24 +0300

    edistyksiä

--- chat-app/public/chat.js
+++ chat-app/public/chat.js
@@ -1,29 +1,31 @@
-"use strict";
+'use strict';
 
 // Server URL below must point to your server, localhost works for local development/testing
-const socket = io("http://localhost:3000");
-const roomDropdown = document.getElementById("room");
+const socket = io('http://localhost:3000');
+/*
+const roomDropdown = document.getElementById('room');
 let selectedRoom;
-roomDropdown.addEventListener("change", function () {
+roomDropdown.addEventListener('change', function () {
   selectedRoom = roomDropdown.value;
 });
-
-document.querySelector("#newsubmit").addEventListener("submit", (event) => {
+*/
+document.querySelector('#newsubmit').addEventListener('submit', (event) => {
   event.preventDefault();
-  const inp = document.getElementById("message");
-  const usernameInp = document.getElementById("username");
+  const inp = document.getElementById('message');
+  const usernameInp = document.getElementById('username');
   const data = {
     username: usernameInp.value,
     msg: inp.value,
-    room: selectedRoom,
+    //  room: selectedRoom,
   };
-  socket.emit("chat message", data);
-  usernameInp.value = "";
-  inp.value = "";
+  socket.emit('chat message', data);
+  inp.value = '';
 });
 
-socket.on("chat message", (data) => {
-  const item = document.createElement("li");
-  item.textContent = data.username + ": " + data.msg;
-  document.getElementById("messages").appendChild(item);
+socket.on('chat message', (data) => {
+  const item = document.createElement('li');
+  const string = data.username + ':' + ' ' + data.msg;
+  item.textContent = string;
+  item.classList.add('mb-4', 'bg-blue-400', 'rounded', 'p-3');
+  document.getElementById('messages').appendChild(item);
 });
