32fc7f09b9e9bf969d3637382cb0b61d775e3c31
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-09-09 21:41:47 +0300

    jepjep

--- chat-app/public/chat.js
+++ chat-app/public/chat.js
@@ -1,7 +1,9 @@
-"use strict";
+'use strict';
 
 // Server URL below must point to your server, localhost works for local development/testing
-const socket = io("https://joonasvm.northeurope.cloudapp.azure.com");
+//const socket = io('https://joonasvm.northeurope.cloudapp.azure.com');
+const socket = io('http://localhost:3000');
+
 /*
 const roomDropdown = document.getElementById('room');
 let selectedRoom;
@@ -9,28 +11,28 @@ roomDropdown.addEventListener('change', function () {
   selectedRoom = roomDropdown.value;
 });
 */
-document.querySelector("#newsubmit").addEventListener("submit", (event) => {
+document.querySelector('#newsubmit').addEventListener('submit', (event) => {
   event.preventDefault();
-  const inp = document.getElementById("message");
-  const usernameInp = document.getElementById("username");
+  const inp = document.getElementById('message');
+  const usernameInp = document.getElementById('username');
   const data = {
     username: usernameInp.value,
     msg: inp.value,
     //  room: selectedRoom,
   };
-  socket.emit("chat message", data);
-  inp.value = "";
+  socket.emit('chat message', data);
+  inp.value = '';
 });
 
-socket.on("chat message", (data) => {
-  const item = document.createElement("li");
-  const string = data.username + ":" + " " + data.msg;
+socket.on('chat message', (data) => {
+  const item = document.createElement('li');
+  const string = data.username + ':' + ' ' + data.msg;
   item.textContent = string;
-  item.classList.add("mb-4", "rounded", "p-3", "text-white");
-  data.username === document.getElementById("username").value
-    ? item.classList.add("self-end", "bg-gmpictonblue")
-    : item.classList.add("self-start", "bg-red-400");
-  document.getElementById("messages").appendChild(item);
-  document.getElementById("messages").scrollTop =
-    document.getElementById("messages").scrollHeight;
+  item.classList.add('mb-4', 'rounded', 'p-3', 'text-white');
+  data.username === document.getElementById('username').value
+    ? item.classList.add('self-end', 'bg-gmpictonblue')
+    : item.classList.add('self-start', 'bg-red-400');
+  document.getElementById('messages').appendChild(item);
+  document.getElementById('messages').scrollTop =
+    document.getElementById('messages').scrollHeight;
 });
