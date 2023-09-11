803c670ab37611878a8554b6947581bba17928db
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-09-11 14:38:31 +0300

    huoneet, last ten messages tehty

--- chat-app/public/chat.js
+++ chat-app/public/chat.js
@@ -3,14 +3,31 @@
 // Server URL below must point to your server, localhost works for local development/testing
 //const socket = io('https://joonasvm.northeurope.cloudapp.azure.com');
 const socket = io('http://localhost:3000');
+const messagesElement = document.getElementById('messages');
 
-/*
 const roomDropdown = document.getElementById('room');
-let selectedRoom;
-roomDropdown.addEventListener('change', function () {
-  selectedRoom = roomDropdown.value;
+let selectedRoom = roomDropdown.value;
+
+socket.on('connect', () => {
+  socket.emit('join room', selectedRoom);
 });
-*/
+
+socket.on('disconnect', () => {
+  socket.emit('leave room', selectedRoom);
+});
+
+roomDropdown.addEventListener('change', function () {
+  // Leave previous room
+  socket.emit('leave room', selectedRoom);
+  selectedRoom = roomDropdown.value;
+  // Clear previous messages
+  while (messagesElement.firstChild) {
+    messagesElement.removeChild(messagesElement.firstChild);
+  }
+  // Join a new room
+  socket.emit('join room', selectedRoom);
+});
+
 document.querySelector('#newsubmit').addEventListener('submit', (event) => {
   event.preventDefault();
   const inp = document.getElementById('message');
@@ -18,7 +35,7 @@ document.querySelector('#newsubmit').addEventListener('submit', (event) => {
   const data = {
     username: usernameInp.value,
     msg: inp.value,
-    //  room: selectedRoom,
+    room: selectedRoom,
   };
   socket.emit('chat message', data);
   inp.value = '';
@@ -32,7 +49,20 @@ socket.on('chat message', (data) => {
   data.username === document.getElementById('username').value
     ? item.classList.add('self-end', 'bg-gmpictonblue')
     : item.classList.add('self-start', 'bg-red-400');
-  document.getElementById('messages').appendChild(item);
-  document.getElementById('messages').scrollTop =
-    document.getElementById('messages').scrollHeight;
+  messagesElement.appendChild(item);
+  messagesElement.scrollTop = messagesElement.scrollHeight;
+});
+
+socket.on('previous messages', (data) => {
+  data.messages.forEach((dataElement) => {
+    const item = document.createElement('li');
+    const string = dataElement.username + ':' + ' ' + dataElement.msg;
+    item.textContent = string;
+    item.classList.add('mb-4', 'rounded', 'p-3', 'text-white');
+    dataElement.username === document.getElementById('username').value
+      ? item.classList.add('self-end', 'bg-gmpictonblue')
+      : item.classList.add('self-start', 'bg-red-400');
+    messagesElement.appendChild(item);
+    messagesElement.scrollTop = messagesElement.scrollHeight;
+  });
 });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,29 +1,57 @@
-"use strict";
+'use strict';
 
 const lastTenMessages = [];
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-io.on("connection", (socket) => {
-  console.log("a user connected", socket.id);
+const rooms = {}; // Dictionary to store chat rooms and their messages
 
-  socket.on("disconnect", () => {
-    console.log("a user disconnected", socket.id);
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected', socket.id);
   });
 
-  socket.on("chat message", (data) => {
-    console.log("message: ", data.msg);
-    console.log("username: ", data.username);
-    //   io.in("data.room").emit(/* ... */);
-    io.emit("chat message", data);
+  socket.on('chat message', (data) => {
+    // Store the message in the room's message history
+    if (!rooms[data.room]) {
+      rooms[data.room] = [];
+    }
+
+    rooms[data.room].push({ username: data.username, msg: data.msg });
+
+    // Limit the number of messages to the last ten
+    if (rooms[data.room].length > 10) {
+      rooms[data.room].shift();
+    }
+
+    //io.in("data.room").emit(/* ... */);
+    // io.emit('chat message', data); KAIKILLE
+
+    io.to(data.room).emit('chat message', data);
+  });
+
+  socket.on('join room', (room) => {
+    socket.join(room);
+    console.log('user joined room' + room);
+
+    if (rooms[room]) {
+      socket.emit('previous messages', { messages: rooms[room] });
+    }
+  });
+
+  socket.on('leave room', (room) => {
+    socket.leave(room);
+    console.log('user left room' + room);
   });
 });
 
 http.listen(3000, () => {
-  console.log("listening on port 3000");
+  console.log('listening on port 3000');
 });
