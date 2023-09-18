87a3b9a65d05bdea2c3c813631a8998bba9dd027
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-09-18 16:18:25 +0300

    dotenv & react scripts

--- video-chat/client/src/App.js
+++ video-chat/client/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import CreateRoom from "./routes/CreateRoom";
-import Room from "./routes/Room";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import CreateRoom from './routes/CreateRoom';
+import Room from './routes/Room';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={CreateRoom} />
-          <Route path="/room/:roomID" component={Room} />
+          <Route path='/' exact component={CreateRoom} />
+          <Route path='/room/:roomID' component={Room} />
         </Switch>
       </BrowserRouter>
     </div>
